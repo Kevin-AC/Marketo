979c9535d08f35d0c24ebd9a6a0dacd3852657c4
kevin.argumedo <kevin.argumedo@gmail.com>
2023-04-30 16:05:14 -0500

    flex centar App

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import Header from './Components/Header'
 import About from './Pages/About'
 import Hero from './Pages/Hero'
 import OurPortfolio from './Pages/OurPortfolio'
+import { OurPricing } from './Pages/OurPricing'
 import Services from './Pages/Services'
 function App () {
   return (
-    <main className="w-screen h-auto bg-white ">
+    <main className="w-screen h-auto bg-white flex flex-col items-center justify-center gap-5 ">
       <Header/>
       <Hero/>
       <Services />
       <About />
       <OurPortfolio />
+      <OurPricing />
     </main>
   )
 }
