378fbb98fb9a2774145fd7132c5923259ec9756b
kevin.argumedo <kevin.argumedo@gmail.com>
2023-05-05 18:02:09 -0500

    header nav pc view

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,10 +3,10 @@ function Header () {
   const [click, setClick] = useState(true)
 
   return (
-    <div className="w-full h-16 bg-white flex justify-between items-center shadow-md sticky top-0 z-50 px-[5%] lg:px-[9%]">
-      <a className="text-2xl font-semibold" href="#">Marketo.</a>
-      {!click && <nav className="bg-white w-[70%] h-auto absolute top-20 right-4 z-50">
-        <ul className="navList p-4 flex flex-col gap-3">
+    <div className="w-full h-16 lg:h-20 bg-white flex justify-between items-center shadow-md sticky top-0 z-50 px-[5%] lg:px-[9%]">
+      <a className="text-2xl font-semibold text-black lg:text-[28px]" href="#">Marketo.</a>
+      <nav className={`navMenu ${!click ? '' : 'hidden lg:flex'}`}>
+        <ul className="navList p-4 flex flex-col gap-3 lg:flex-row">
           <li><a href="#">Home</a></li>
           <li><a href="#">Services</a></li>
           <li><a href="#">About</a></li>
@@ -15,7 +15,7 @@ function Header () {
           <li><a href="#">Review</a></li>
           <li><a href="#">Contact</a></li>
         </ul>
-      </nav>}
+      </nav>
       <div onClick={() => setClick(!click)} className={`Hmenu fas  ${click ? 'fa-bars' : 'fa-times'}`} ></div>
     </div>
   )
