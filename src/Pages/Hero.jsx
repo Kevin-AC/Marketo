f4f8749611e4dc6381e393b7fa77deea5c701236
kevin.argumedo <kevin.argumedo@gmail.com>
2023-05-05 17:39:00 -0500

    Hero pc view

--- src/Pages/Hero.jsx
+++ src/Pages/Hero.jsx
@@ -1,18 +1,20 @@
 import Button from '../Components/Button'
 function Hero () {
   return (
-    <section className='bg-bg relative pt-7 -mt-5'>
-      <div className="flex flex-col items-center gap-6 mb-4">
-        <h3 className="text-3xl font-semibold text-center">grow your business with us</h3>
-        <p className="text-lightColor text-center text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    <section className='bg-bg relative pt-7 -mt-5 w-full'>
+      <div className="flex flex-col items-center gap-6 mb-4 text-center lg:text-left lg:items-start">
+        <h3 className="text-3xl font-semibold lg:text-4xl ">grow your business with us</h3>
+        <p className="text-lightColor  text-sm lg:text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit.
            Facere atque sapiente veritatis nesciunt adipisci culpa.</p>
         <Button text='Try For Free' />
       </div>
-     <div className='w-full h-auto'>
-        <img src="/assets/rocket.svg" alt="Rocket image" />
-     </div>
-    <div className='cloud '></div>
-    <div className="cloud cloud-1"></div>
+      <div className='w-full h-auto '>
+        <div className='w-full h-auto'>
+          <img src="/assets/rocket.svg" alt="Rocket image" />
+        </div>
+        <div className='cloud '></div>
+        <div className="cloud cloud-1"></div>
+    </div>
     </section>
   )
 }
