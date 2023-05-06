93ddfdf867e591e3d5cbecdb23ddb2422632471e
kevin.argumedo <kevin.argumedo@gmail.com>
2023-05-05 19:47:01 -0500

    About pc view

--- src/Components/Title.jsx
+++ src/Components/Title.jsx
@@ -1,8 +1,8 @@
 // eslint-disable-next-line react/prop-types
 function Title ({ title, span, style }) {
   return (
-    <h1 className={`text-3xl font-semibold text-black my-4  ${style}`}>{title}
-    <span className='text-mainColor bg-bg px-2 rounded-lg'>{span}</span>
+    <h1 className={`text-3xl lg:text-[44px] font-semibold text-black my-4 items-center gap-2 ${style}`}>{title}
+    <span className='text-mainColor bg-bg p-1 rounded-lg'>{span}</span>
     </h1>
   )
 }
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -5,32 +5,30 @@ import Title from '../Components/Title'
 function About () {
   return (
     <section>
-      <Title title='Us' span='About' style='flex flex-row-reverse ' />
-      <div className='flex flex-col lg:flex-row gap-4'>
-        <img src="/assets/about-img.png" alt="about ilustration" />
+      <Title title='Us' span='About' style='flex flex-row-reverse  ' />
+      <div className='flex flex-col items-center mt-8 sm:flex-row gap-4 lg:gap-8'>
+        <img className='w-auto sm:w-96 lg:w-[450px]' src="/assets/about-img.png" alt="about ilustration" />
         <div className='flex flex-col gap-4'>
-          <h3 className='text-black font-semibold text-2xl'>big projects start with big dreams</h3>
-          <p className='text-left text-sm text-lightColor'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias blanditiis, fugiat consequatur enim amet ullam facere quaerat, quos quae mollitia inventore! Hic, excepturi debitis velit maxime ut in at iusto.</p>
+          <h3 className='text-black font-semibold text-2xl lg:text-3xl'>big projects start with big dreams</h3>
+          <p className='text-left text-sm text-lightColor lg:text-base'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias blanditiis, fugiat consequatur enim amet ullam facere quaerat, quos quae mollitia inventore! Hic, excepturi debitis velit maxime ut in at iusto.</p>
           <Button text='Learn More' style='w-32' />
-         <div className='flex flex-wrap gap-2 '>
-            <div className='flex-auto'>
+         <div className='w-full grid grid-cols-2 sm:grid-cols-3  gap-2'>
               <Card
                 icon='fas fa-award'
                 title='14 Award Won'
                 styleT='text-xs' />
-            </div>
-            <div className='flex-auto'>
+
               <Card
                 icon='fas fa-user'
                 title='250 satisfied clients'
                 styleT='text-xs' />
-            </div>
-            <div className='flex-1'>
+
+            <div className='col-span-3 sm:col-auto'>
               <Card
                 icon='fas fa-project-diagram'
                 title='200 projects completed'
                 styleT='text-xs' />
-           </div>
+             </div>
          </div>
         </div>
       </div>
